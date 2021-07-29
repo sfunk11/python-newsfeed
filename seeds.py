f21b62b845be19b067f61c56a5f0e273a38187c6
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-07-29 09:36:44 -0400

    added validation and encryption to users table

--- seeds.py
+++ seeds.py
@@ -9,7 +9,7 @@ db = Session()
 
 # insert users
 db.add_all([
-  User(username='alesmonde0', email='nwestnedge0@cbc.ca', password='password123'),
+  User(username='alesmonde0', email='email@email.edu', password='password123'),
   User(username='jwilloughway1', email='rmebes1@sogou.com', password='password123'),
   User(username='iboddam2', email='cstoneman2@last.fm', password='password123'),
   User(username='dstanmer3', email='ihellier3@goo.ne.jp', password='password123'),
