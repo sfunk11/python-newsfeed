d5340ff0f60ca0898a499ffdb0124ed8d38e9f16
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-08-01 18:51:22 -0400

    added sessions

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, session
 from app.models import User
 from app.db import get_db
+import sys
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -9,13 +10,24 @@ def signup():
   data = request.get_json()
   db = get_db()
 
-  newUser = User(
+  try:
+    newUser = User(
     username = data['username'],
     email = data['email'],
     password = data['password']
   )	
  # save in database
-  db.add(newUser)
-  db.commit()	
+    db.add(newUser)
+    db.commit()	
 
+  except:
+    print(sys.exc_info()[0])
+
+  # insert failed, so rollback and send error to front end
+    db.rollback()
+    return jsonify(message = 'Signup failed'), 500
+  
+  session.clear()
+  session['user_id'] = newUser.id
+  session['loggedIn'] = True
   return jsonify(id = newUser.id)
