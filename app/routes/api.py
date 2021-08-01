920d5e8363defce2a86ca01fe73789ad7a403152
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-08-01 18:05:03 -0400

    created the api routes file

---
+++ app/routes/api.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, request
+from app.models import User
+from app.db import get_db
+
+bp = Blueprint('api', __name__, url_prefix='/api')
+
+@bp.route('/users', methods=['POST'])
+def signup():
+
+  return ''
