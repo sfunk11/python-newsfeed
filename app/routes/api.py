73707c11510779c5e628c2aff892175358322a98
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-08-02 11:08:43 -0400

    protected routes to require login

--- app/routes/api.py
+++ app/routes/api.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify, session
 from app.models import User, Post, Comment, Vote
 from app.db import get_db
 import sys
+from app.utils.auth import login_required
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -58,6 +59,7 @@ def login():
   return jsonify(id = user.id)
 
 @bp.route('/comments', methods=['POST'])
+@login_required
 def comment():
   data = request.get_json()
   db = get_db()
@@ -80,6 +82,7 @@ def comment():
   return jsonify(id = newComment.id)
 
 @bp.route('/posts/upvote', methods=['PUT'])
+@login_required
 def upvote():
   data = request.get_json()
   db = get_db()
@@ -102,6 +105,7 @@ def upvote():
   return '', 204
 
 @bp.route('/posts', methods=['POST'])
+@login_required
 def create():
   data = request.get_json()
   db = get_db()
@@ -125,6 +129,7 @@ def create():
   return jsonify(id = newPost.id)
 
 @bp.route('/posts/<id>', methods=['PUT'])
+@login_required
 def update(id):
   data = request.get_json()
   db = get_db()
@@ -143,6 +148,7 @@ def update(id):
   return '', 204
 
 @bp.route('/posts/<id>', methods=['DELETE'])
+@login_required
 def delete(id):
   db = get_db()
 
